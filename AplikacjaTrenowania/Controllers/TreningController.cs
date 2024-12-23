ee7735a201007662b2493a54d882ce8d6bbc1f07
Imie Nazwisko <me@imienazwisko.com>
2024-12-23 16:10:36 +0100

    Klasa trening, Klasa seria, widoki, na podstawie filmiku z lekcji o ASP.NET MVC EF, zakończone niepowodzeniem

---
+++ AplikacjaTrenowania/Controllers/TreningController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AplikacjaTrenowania.Models;
+
+namespace AplikacjaTrenowania.Controllers
+{
+    public class TreningController : Controller
+    {
+        // GET: TreningController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: TreningController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TreningController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TreningController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TreningController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TreningController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TreningController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TreningController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ AplikacjaTrenowania/Models/Seria.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AplikacjaTrenowania.Models
+{
+    public class Seria
+    {
+        [Key]
+        public int IdSerii { get; set; }
+        [Column(TypeName = "int")]
+        public int Kg { get; set; }
+        [Column(TypeName = "int")]
+        public int Powtorzenia { get; set; }
+    }
+}

20b9efd8d12ef0ff90e93a666d33bdbba697389d
Damian Sikora <me@imienazwisko.com>
2025-01-08 18:10:59 +0100

    Dodanie opisu na dzien 07.01.2024, przeniesienie skryptów do osobnych plików

---
+++ AplikacjaTrenowania/wwwroot/js/deleteworkout.js
@@ -0,0 +1,22 @@
+﻿function deleteWorkout(id, button) {
+    console.log(id);
+    const dane = {
+        id: id
+    };
+    const url = "/Trening/Delete/${id}";
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Requested-With': 'XMLHttpRequest'
+        },
+        body: id
+    })
+        .then(response => {
+            if (response.ok) {
+                const row = button.closest('tr');
+                if (row) row.remove();
+            }
+        })
+        .catch(error => console.error('Błąd w żądaniu:', error));
+}
---
+++ AplikacjaTrenowania/wwwroot/js/expandingist.js
@@ -0,0 +1,16 @@
+﻿document.querySelectorAll(".rozwijanie-przycisk").forEach(button => {
+        button.addEventListener("click", function () {
+            let nextRow = this.closest("tr").nextElementSibling;
+            while (nextRow && !nextRow.classList.contains("rozwijanie-przycisk-wiersz")) {
+                if (nextRow.classList.contains("hidden")) {
+                    nextRow.classList.remove("hidden");
+                    // nextRow.classList.add("visible");
+                } else {
+                    // nextRow.classList.remove("visible");
+                    nextRow.classList.add("hidden");
+                }
+                nextRow = nextRow.nextElementSibling;
+            }
+            this.textContent = this.textContent === "+" ? "-" : "+";
+        });
+    });
